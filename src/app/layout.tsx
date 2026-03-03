f8c4910fec0d22702479b0c616b6752b5497bf08
caio <caiodiasol@proton.me>
2026-03-03 15:55:20 -0300

    feat: implement reading progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { ScrollProgress } from "./components/scroll-progress";
 
 const geistSans = Inter({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
             `,
           }}
         />
+        <ScrollProgress />
         {children}
       </body>
     </html>
